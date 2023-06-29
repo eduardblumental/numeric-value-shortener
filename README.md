d803ae37efa7d1b5ed47d2d49515ad05faefedb6
Eduards <eduard.blumental@gmail.com>
2023-06-29 10:12:21 +0300

    Description added. Minor fixes introduced.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# zoomcharts-qa-task
-Zoomcharts QA Homework Assignment
+# Numeric Value Shortener
+
+## Project Description
+
+This JavaScript project includes a module `valueShortener.test.js` which contains a utility function `shortening(value, desiredValueUnit)` for shortening numeric values to their equivalent representation in different units such as "K" (thousands), "M" (millions), "bn" (billions), and "T" (trillions).
+
+The function takes two arguments: `value` and `desiredValueUnit`. The `value` is the numeric input you want to shorten, and the `desiredValueUnit` is the unit you want your input to be converted to. The function returns an object with the shortened value and the unit.
+
+A series of automated tests are also included in `valueShortener.test.js` to ensure the function behaves as expected for different edge cases. The tests are written using Jest, a popular JavaScript testing framework.
+
+## Prerequisites
+
+To run this code and its tests, you need to have Node.js and npm installed on your machine. You can download and install them from [here](https://nodejs.org/).
+
+## Setup
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory in your terminal.
+3. Run `npm install` to install the dependencies listed in `package.json`.
+
+## Running the Tests
+
+To run the tests, simply use the command `npm test` in your terminal while in the project directory. This will start the Jest test runner and run all the tests defined in `valueShortener.test.js`.
--- js/valueShortener.test.js
+++ js/valueShortener.test.js
@@ -1,57 +1,57 @@
-const { shortening } = require('./converter.js');
+const { shortening } = require('./valueShortener.js');
 
 describe('Converts to specific unit', () => {
-  test('shortening(123456789, "") -> 123456789', () => {
+  test('shortening(123456789, "") -> 123456789, ""', () => {
     expect(shortening(123456789, "")).toEqual({ value: 123456789, valueUnit: "" });
   });
 
-  test('shortening(123456789, "K") -> 123456.789 K', () => {
+  test('shortening(123456789, "K") -> 123456.789, "K"', () => {
     expect(shortening(123456789, "K")).toEqual({ value: 123456.789, valueUnit: "K" });
   });
 
-  test('shortening(123456789, "M") -> 123.456789 M', () => {
+  test('shortening(123456789, "M") -> 123.456789, "M"', () => {
     expect(shortening(123456789, "M")).toEqual({ value: 123.456789, valueUnit: "M" });
   });
 
-  test('shortening(123456789, "bn") -> 0.123456789 bn', () => {
+  test('shortening(123456789, "bn") -> 0.123456789, "bn"', () => {
     expect(shortening(123456789, "bn")).toEqual({ value: 0.123456789, valueUnit: "bn" });
   });
 
-  test('shortening(123456789, "T") -> 0.000123456789 T', () => {
+  test('shortening(123456789, "T") -> 0.000123456789, "T"', () => {
     expect(shortening(123456789, "T")).toEqual({ value: 0.000123456789, valueUnit: "T" });
   });
 });
 
 describe('Converts to largest whole unit if unit is undefined or null', () => {
-  test('shortening(10) -> 10', () => {
+  test('shortening(10) -> 10, ""', () => {
     expect(shortening(10)).toEqual({ value: 10, valueUnit: "" });
   });
 
-  test('shortening(10000) -> 10 K', () => {
+  test('shortening(10000) -> 10, "K"', () => {
     expect(shortening(10000)).toEqual({ value: 10, valueUnit: "K" });
   });
 
-  test('shortening(10000000) -> 10 M', () => {
+  test('shortening(10000000) -> 10, "M"', () => {
     expect(shortening(10000000)).toEqual({ value: 10, valueUnit: "M" });
   });
 
-  test('shortening(10000000000) -> 10 bn', () => {
+  test('shortening(10000000000) -> 10, "bn"', () => {
     expect(shortening(10000000000)).toEqual({ value: 10, valueUnit: "bn" });
   });
 
-  test('shortening(10000000000000) -> 10 T', () => {
+  test('shortening(10000000000000) -> 10, "T"', () => {
     expect(shortening(10000000000000)).toEqual({ value: 10, valueUnit: "T" });
   });
 });
 
 describe('Returns original value and unit if unit is invalid', () => {
-  test('shortening(1234, "invalid unit") -> 1234, invalid unit', () => {
+  test('shortening(1234, "invalid unit") -> 1234, "invalid unit"', () => {
     expect(shortening(1234, "invalid unit")).toEqual({ value: 1234, valueUnit: "invalid unit" });
   });
 });
 
 describe('Returns original value and unit if value is invalid', () => {
-  test('shortening(undefined, "K") -> undefined, K', () => {
+  test('shortening(undefined, "K") -> undefined, "K"', () => {
     expect(shortening(undefined, "K")).toEqual({ value: undefined, valueUnit: "K" });
   });
 });
@@ -75,7 +75,7 @@ describe('Handles negative values', () => {
 });
 
 describe('Handles non-numeric values', () => {
-  test('shortening("test") -> test, ""', () => {
-    expect(shortening("test")).toEqual({ value: "test", valueUnit: "" });
+  test('shortening("non-numeric") -> "non-numeric", ""', () => {
+    expect(shortening("non-numeric")).toEqual({ value: "non-numeric", valueUnit: "" });
   });
 });
